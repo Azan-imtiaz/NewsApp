9f946bf822f7cc5aa53692a5fcd0a216b5792eef
Azan-imtiaz <azanimtiaz43@gmail.com>
2023-10-17 22:28:05 +0500

    serverless function

--- functions/news.js
+++ functions/news.js
@@ -5,9 +5,10 @@ exports.handler = async (event, context) => {
 const API_KEY="1756bd1a6bce4492b2312060a7eb8f80";
     const query = event.queryStringParameters.q;
 
-    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-09-17&sortBy=publishedAt&apiKey=${API_KEY}`;
+    // const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-09-17&sortBy=publishedAt&apiKey=${API_KEY}`;
+    const apiUrl = `https://newsapi.org/v2/everything?q="pak"&apiKey=1756bd1a6bce4492b2312060a7eb8f80`;
     // https://newsapi.org/v2/everything?q=tesla&from=2023-09-17&sortBy=publishedAt&apiKey=1756bd1a6bce4492b2312060a7eb8f80
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl,{method:"GET"});
     const data = await response.json();
 
     return {
